a11c971d41502d95ba75bc1aa36fd0e352c52e92
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2023-04-21 14:32:49 +0700

    connect to mongo

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -20,3 +21,9 @@ const server = http.createServer(app);
 server.listen(8000, () => {
   console.log("Server is running on http://localhost:8000/");
 })
+
+const MONGO_URL = "mongodb+srv://aksaamerta:nadia123@cluster0.eic04nq.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
