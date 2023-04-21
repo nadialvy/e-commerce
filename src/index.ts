c86875a05416b7a5be7e1d0526bc4f96aa4837cd
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2023-04-21 14:23:46 +0700

    setup express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('halo');
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+  credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8000, () => {
+  console.log("Server is running on http://localhost:8000/");
+})
